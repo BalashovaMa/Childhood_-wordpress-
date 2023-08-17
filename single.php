0e999158ba41dc380459b130dc25a4cba792b702
BalashovaMa <balashova.m.nd@gmail.com>
2023-08-17 14:13:51 +0300

    feat: add single page for toy

--- single.php
+++ single.php
@@ -1,21 +1,29 @@
-<?php 
-/*
-    Template Name: Toys
-*/
-?>
-
 <?php
     get_header();
 ?>
 
-        <div class="toys">
-            <div class="container">
-                <h2 class="subtitle">Soft toys</h2>
+
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+    <div class="container toys">
+                <h2 class="subtitle">maybe you will like it</h2>
         
                 <div class="toys__wrapper">
                     <?php 
                         $posts = get_posts( array(
-                            'numberposts' => -1,
+                            'numberposts' => 3,
                             'category_name'    => 'soft_toys',
                             'orderby'     => 'date',
                             'order'       => 'ASC',
@@ -38,7 +46,7 @@
                                 <div class="toys__item-descr">
                                     <?php the_field('description_toys'); ?>                           
                                 </div>
-                            <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Read More</a>
+                                <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Read More</a>
                         </div>
                     </div>
                     
@@ -49,43 +57,7 @@
                     ?>
                 </div>
 
-                <h2 class="subtitle">Education toys</h2>
-        
-                <div class="toys__wrapper">
-                    <?php 
-                        $posts = get_posts( array(
-                            'numberposts' => -1,
-                            'category_name'    => 'edu_toys',
-                            'orderby'     => 'date',
-                            'order'       => 'ASC',
-                            'post_type'   => 'post',
-                            'suppress_filters' => true, 
-                        ) );
-
-                        foreach( $posts as $post ){
-                            setup_postdata($post);
-                        ?>
-
-                        <div class="toys__item" style="background-image: url(<?php 
-                        if (has_post_thumbnail( )) {
-                            the_post_thumbnail_url( );
-                        } else {
-                            echo get_template_directory_uri() . '/assets/img/not-found.png';
-                        }?>)">
-                        <div class="toys__item-info">
-                        <div class="toys__item-title"><?php the_title(); ?></div>
-                        <div class="toys__item-descr">
-                            <?php the_field('description_toys'); ?>                           
-                        </div>
-                    <a href="<?php echo get_permalink(); ?>" class="minibutton toys__trigger">Read More</a>
-                </div>
-            </div>
-            
-            <?php
-                }
-
-                wp_reset_postdata(); 
-            ?>
+               
 
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
@@ -95,8 +67,7 @@
                     </div>
                 </div>
             </div>
-        </div>
 
 <?php
+
     get_footer();
-?>
